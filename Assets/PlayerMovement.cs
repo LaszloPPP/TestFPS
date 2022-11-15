d99de966f760dec269ce9c9e40f63d28630036b0
LaszloPPP <laszlo.k.papp@gmail.com>
2022-11-15 15:49:15 +0100

    Sprint
    Sprint Added to PlayerMovement script

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,17 +7,49 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
     public CharacterController controller;
-    // Start is called before the first frame update
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance=1f;
+    public LayerMask groundMask;
 
 
-    // Update is called once per frame
+    Vector3 velocity;
+    bool isGrounded;
+
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        bool sprint = Input.GetKey(KeyCode.LeftShift);
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if (sprint == true)
+        {
+            controller.Move(move * speed * 1.3f * Time.deltaTime);
+        }
+
+        
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
